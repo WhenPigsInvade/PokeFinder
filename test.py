460a636f770842bbd41c46e76a966f9de43f246b
WhenPigsInvade <32582535+WhenPigsInvade@users.noreply.github.com>
2022-04-25 15:28:52 -0700

    Resizes image to decrease searchtime
    Also now checks for flipped picture

--- test.py
+++ test.py
@@ -4,7 +4,8 @@ import os
 import time
 
 # read game image
-img = cv2.imread('source.jpg')
+img = cv2.imread('source_yamper.jpg')
+img = cv2.resize(img, (0,0), fx=0.2, fy=0.2)
 
 start = time.time()
 
@@ -14,9 +15,12 @@ for filename in os.listdir(path_of_the_directory):
     if os.path.isfile(f):
 
         min_loc = 0
-        for size in np.linspace(0.65, 0.85, 5):
+        threshhold = 0.96
+
+        picture = cv2.imread(f, cv2.IMREAD_UNCHANGED)
+        for size in np.linspace(0.13, 0.18, 11):
             # read bananas image template
-            template = cv2.resize(cv2.imread(f, cv2.IMREAD_UNCHANGED), (0,0), fx=size, fy=size)
+            template = cv2.resize(picture, (0,0), fx=size, fy=size)
             hh, ww = template.shape[:2]
 
             # extract bananas base image and alpha channel and make alpha 3 channels
@@ -28,26 +32,44 @@ for filename in os.listdir(path_of_the_directory):
             correlation = cv2.matchTemplate(img, base, cv2.TM_CCORR_NORMED, mask=alpha)
 
             # set threshold and get all matches
-            threshhold = 0.95
             loc = np.where(correlation >= threshhold)
 
 
 
             # found the best fit
-            if len(loc[0]) != 0 and len(loc[0]) < 200 and len(loc[0]) > min_loc:
+            if len(loc[0]) != 0 and len(loc[0]) < 200:
                 print(filename)
                 print(len(loc[0]))
-                print(size - 0.01)
-                break
-            elif len(loc[0]) != 0:
-##                print(len(loc[0]))
-                min_loc = len(loc[0])
+                print(size)
 
-    ##            result = img.copy()
-    ##            for pt in zip(*loc[::-1]):
-    ##                cv2.rectangle(result, pt, (pt[0]+ww, pt[1]+hh), (0,0,255), 1)
-    ##            cv2.imshow('result',result)
-    ##            cv2.waitKey(0)
-    ##            cv2.destroyAllWindows()
+
+        #Now flipped
+        picture = cv2.flip(picture, 1)
+        for size in np.linspace(0.13, 0.18, 11):
+            # read bananas image template
+            template = cv2.resize(picture, (0,0), fx=size, fy=size)
+            hh, ww = template.shape[:2]
+
+            # extract bananas base image and alpha channel and make alpha 3 channels
+            base = template[:,:,0:3]
+            alpha = template[:,:,3]
+            alpha = cv2.merge([alpha,alpha,alpha])
+
+            # do masked template matching and save correlation image
+            correlation = cv2.matchTemplate(img, base, cv2.TM_CCORR_NORMED, mask=alpha)
+
+            # set threshold and get all matches
+            loc = np.where(correlation >= threshhold)
+
+
+
+            # found the best fit
+            if len(loc[0]) != 0 and len(loc[0]) < 200:
+                print(filename)
+                print(len(loc[0]))
+                print(size)
+                print('flipped')
+
+
 
 print(time.time() - start)
